5455ae4ad6f0065207ca842cdd373a84b099d898
s251388 <s251388@students.aie.edu.au>
2026-03-04 00:49:05 +1100

    Comments and asserts
    Added asserts for 3 targets that exist in the array and 1 that does not.
    Also added a couple comments.

--- AIE-2.2.1-Linear-Search/AIE-2.2.1-Linear-Search.cpp
+++ AIE-2.2.1-Linear-Search/AIE-2.2.1-Linear-Search.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -9,8 +10,8 @@ int LinearSearch(int Target, int Array[], size_t Size) {
 	for (int i = 0; i < Size; i++) {
 
 		if (Array[i] == Target) {
-			return i;
-		}
+			return i;						// Another return here is safe enough to use because it immediately kills the
+		}									// function, meaning the loop doesn't continue, and the -1 return never runs.
 
 	}
 
@@ -21,4 +22,12 @@ int LinearSearch(int Target, int Array[], size_t Size) {
 int main()
 {
     
+	int IntArray[20] {67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29};
+	size_t ArraySize = sizeof(IntArray) / sizeof(IntArray[0]);				// The assessment told us the array will have 20 ints, but i'm trying to keep
+																			// this modular anyways by calculating the array size instead of assuming it.
+	assert(LinearSearch(3, IntArray, ArraySize) == 2);
+	assert(LinearSearch(7, IntArray, ArraySize) == 11);
+	assert(LinearSearch(67, IntArray, ArraySize) == 0);
+	assert(LinearSearch(88, IntArray, ArraySize) == -1);
+
 }
